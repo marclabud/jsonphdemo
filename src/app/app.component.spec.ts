285ff412bf91d38b374c38628a9f17409b619f71
Marc Labud <marc.labud@hsr.ch>
2018-11-24 14:15:30 +0100

    Setup Application in Material Design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+        imports: [
+            RouterTestingModule,
+            BrowserModule,
+            AppRoutingModule,
+            BrowserAnimationsModule,
+            LayoutModule,
+            MatToolbarModule,
+            MatButtonModule,
+            MatSidenavModule,
+            MatIconModule,
+            MatListModule
+
+        ],
       declarations: [
-        AppComponent
+          AppComponent,
+          NavbarComponent
       ],
     }).compileComponents();
   }));
