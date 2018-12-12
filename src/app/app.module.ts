54c4998875db934fbbc8489de6bfc11747b6cc8e
Marc Labud <marc.labud@hsr.ch>
2018-12-12 17:16:42 +0100

    refactor: material-module setup, hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NavbarComponent} from './navbar/navbar.component';
-import {LayoutModule} from '@angular/cdk/layout';
-import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {TodoComponent} from './todo/todo.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
-import { UserModule } from './user/user.module';
+import {MaterialModule} from './material-module';
+import {UserModule} from './user/user.module';
+
 
 @NgModule({
     declarations: [
@@ -24,13 +24,8 @@ import { UserModule } from './user/user.module';
         AppRoutingModule,
         HttpClientModule,
         BrowserAnimationsModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        MaterialModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
         UserModule
     ],
     providers: [],
